6480bc75e36404750bfb5dcb67082273f80256c8
shu-silent <134462148+shu-silent@users.noreply.github.com>
2023-11-01 13:34:00 +0900

    スクレイピング3

--- resources/views/books/detail.blade.php
+++ resources/views/books/detail.blade.php
@@ -12,21 +12,21 @@
 @section('content')
     
     
-    @if(session('relatedItems'))
-    <ul>
+@if(session('relatedItems'))
+    <ul style="list-style: none; padding: 0; display: flex; flex-direction: column; align-items: center; gap: 20px;">
         @foreach(session('relatedItems') as $item)
-            <li>
-                <a href="{{ $item->url }}" style="text-decoration: none; color: inherit; display: block; width: 100%; height: 100%;" class="btn btn-default">
-                    <div style="width: 500px; height: 130px; ">
-                        <div style="width: 33.33%; height: 100%; float: left;">
-                            <img src="{{ $item->ogp['image'] }}" alt="No Image" style="max-width: 100%; max-height: 100%; object-fit: cover;">
+            <li style="width: 500px; margin-bottom: 20px; position: relative;"> <!-- セットのdivの大きさを可変、上下の余白追加 -->
+                <a href="{{ $item->url }}" class="btn btn-default" style="text-decoration: none; color: inherit; display: block;">
+                    <div style="width: 100%; height: 100%; padding: 10px; display: flex; gap: 10px;">
+                        <div style="width: 30%; height: 100%;">
+                            <img src="{{ $item->ogp['image'] }}" alt="No Image" style="width: 100%; height: 100%; object-fit: cover;">
                         </div>
-                        <div style="width: 66.67%; height: 100%; float: left;">
-                            <div style="width: 100%; height: 30%; font-weight: bold;">
+                        <div style="width: 70%; display: flex; flex-direction: column;">
+                            <div style="height: 40%; text-align: center; padding: 5px; font-weight: bold;">
                                 {{ $item->ogp['title'] }}
                             </div>
-                            <div style="height: 70%; width: 100%; overflow: hidden; position: relative;">
-                                <div style="height: 100%; position: absolute; top: 0; left: 0; background: linear-gradient(to right, rgba(255, 255, 255, 0), white 70%);"></div>
+                            <div style="height: 60%; text-align: center; padding: 5px; position: relative;">
+                                <div style="height: 100%; position: absolute; top: 0; left: 0; background: linear-gradient(to top, transparent 0%, #f1f1f1 100%);"></div>
                                 {{ $item->ogp['description'] }}
                             </div>
                         </div>
@@ -35,9 +35,10 @@
             </li>
         @endforeach
     </ul>
-    @else
-        <p>No related items found.</p>
-    @endif
+@else
+    <p>No related items found.</p>
+@endif
+
 
     <!-- ポップアップのHTML -->
     <div id="popup" class="card card-primary" style="display: none; position: fixed; top: 50%; left: 50%; transform: translate(-50%, -50%); background: white; padding: 20px; border: 1px solid #ccc; width: 600px; z-index: 2;">
